f1ddee32c29acc010a0173d0e39ae3c1ae88dc30
Vitor Da Silveira Coelho <vitorcoelhoft@gmail.com>
2024-10-01 22:14:16 -0300

    First in go

---
+++ goproject/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	if retorno, err := test(); err != nil {
+		fmt.Println("Houve erro")
+	}
+ 
+	// user := user{
+	// 	name:  "vitor",
+	// 	age:   17,
+	// 	test2: "test2",
+	// }
+
+	// fmt.Println(user)
+
+	// testJson := map[string]interface{}{
+
+	// 	"nome":  "vitor",
+	// 	"idade": 25,
+	// }
+
+	// fmt.Printf("%T", testJson)
+
+	//test := 30.0
+	//fmt.Printf("%T", test)
+
+}
+
+func test
+
+// type user struct {
+	// name  string
+	// age   int
+	// test2 string
+// }
