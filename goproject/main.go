38c617c57b11d3e31b953503fc19a2d18af979a3
Vitor Da Silveira Coelho <vitorcoelhoft@gmail.com>
2024-10-09 22:55:41 -0300

    folders structures

--- goproject/main.go
+++ goproject/main.go
@@ -1,39 +1,33 @@
 package main
 
-import (
-	//"errors"
-	"fmt"
-)
+/*
 
-//funcao anonima
+Estrutura de pastas e GOPATH
+~/go
+/pkg lugar onde o guarda todas as dependencias já instaladas
+em seus projeto.
+
+primeiro ele da um git clone e joga dentro da /pkg
+
+/bin lugar onde o go guarda os binarios que "go install" compila
+
+$GOROOT é onde fica guardado o código go, literalmente.
+
+go modules - é uma coleção de pacotes go guardado com o nome de go.mod
+
+//indirect na frente do pacote, quer dizer que alguma de suas dependencias
+dependem desse pacote no qual está marcado.
+
+go.sum aponta os hash dos commit e os pacotes. ele é autogerado.const
+
+pseudo versoes
+
+
+
+
+
+*/
 
 func main() {
 
-	test()
-
 }
-
-func test() {
-
-	func(valorString string, valorInt int) {
-		fmt.Println(valorString, valorInt)
-	}("otavio", 20)
-}
-
-//funcao
-
-// func main() {
-
-// 	value, err := test()
-
-// 	if err != nil {
-// 		fmt.Println(err)
-// 	}
-
-// 	fmt.Println(value)
-
-// }
-
-// func test() (string, error) {
-// 	return "", errors.New("test")
-// }
